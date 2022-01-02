0aea7c686f70a438a20748c9a77e16acf9cfd913
Hein <heindauven@gmail.com>
2022-01-02 01:20:04 +0100

    Update static site generator plugin configuration.

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,23 @@
 /** @type {import('@sveltejs/kit').Config} */
 import adapter from '@sveltejs/adapter-static';
+const dev = process.env.NODE_ENV === 'development';
 
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 		vite: {
 			optimizeDeps: {
 				exclude: ['@urql/svelte'],
 			}
 		},
 		paths: {
-			base: '/iinsight',
-			assets: '/iinsight'
+			base: dev ? '' : '',
 		},
 	},
 };
